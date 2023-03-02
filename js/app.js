c043f6d9dc4f67a849627140a86d9d8229958c62
Mike Warren <37048138+mjwarren3@users.noreply.github.com>
2023-03-02 05:22:19 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// import Typed from "./typed.js@2.0.12";
+
+// // Var is global, let and const are scoped locally
+
+// var typed = new Typed("#typed", {
+//     strings: ["Attractions", "Restaurants", "Events"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+// }); // Declare a new Typed class object, with the selector, and definitions
+
+let typedval = document.getElementById("typed");
+typedval.innerText = "Restaurants";
